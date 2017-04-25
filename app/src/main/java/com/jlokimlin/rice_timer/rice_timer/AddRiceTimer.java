4b8393da0df1b1ccc137a1a2635914fe2e62d0f5
jlokimlin <jlokimlin@gmail.com>
2017-04-24 21:09:44 -0700

    Style and LINT improvements

--- app/src/main/java/com/jlokimlin/rice_timer/rice_timer/AddRiceTimer.java
+++ app/src/main/java/com/jlokimlin/rice_timer/rice_timer/AddRiceTimer.java
@@ -15,21 +15,25 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 import bunny.yunny.com.rice_timer.R;
 import shortbread.Shortbread;
 import shortbread.Shortcut;
 
-
+/**
+ *
+ */
 public class AddRiceTimer extends Activity implements SeekBar.OnSeekBarChangeListener{
 
-    public static final int REST_RICE = 25;
-    public static final int COOK_RICE = 15;
-    public static final float BRING_RICE_TO_BOIL = 3.5f;
+    private static final int REST_RICE = 25;
+    private static final int COOK_RICE = 15;
+    private static final float BRING_RICE_TO_BOIL = 3.5f;
+    public static final int SLEEP_TIME_MILLIS = 1000;
+    public static final int SECONDS_IN_MINUTE = 60;
     private ExecutorService singleThreadPool;
     private Map<Integer, String> timers = new HashMap<>();
     private TextView intervalTextView;
+    public static final int THREE_MINUTES = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,34 +43,18 @@ public class AddRiceTimer extends Activity implements SeekBar.OnSeekBarChangeLis
         singleThreadPool = Executors.newFixedThreadPool(1);
     }
 
-    public void setTimer(final String message, final float minutes) {
-        singleThreadPool.submit(new Runnable() {
-            public void run() {
-                //TimeUnit.SECONDS.convert(minutes, TimeUnit.MINUTES);
-                int seconds = (int)(60 * minutes);
-                Intent met = new Intent(AlarmClock.ACTION_SET_TIMER);
-                met.putExtra(AlarmClock.EXTRA_LENGTH, seconds);
-                met.putExtra(AlarmClock.EXTRA_MESSAGE, message);
-                met.putExtra(AlarmClock.EXTRA_SKIP_UI, true);
-                startActivityForResult(met,seconds);
-                Log.v("Rice timer", "Set timer of len " + seconds + " seconds with message " + message);
-            }
-        });
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Log.v("ActivityResult", "Timer started. Request Code: " + requestCode + " Result: " + resultCode);
     }
 
-    public void metronome(View w) {
-        int three_minutes = 3;
-        int timer_length = three_minutes;
+    public void metronome(View v) {
+        int timer_length = THREE_MINUTES;
         for (int i = 0; i < 16; i++) {
             try {
-                Thread.sleep(1000);
+                Thread.sleep(SLEEP_TIME_MILLIS);
                 setTimer("Flip " + i, timer_length);
-                timer_length = timer_length + three_minutes;
+                timer_length = timer_length + THREE_MINUTES;
             } catch(Exception e) {
                 e.printStackTrace();
             }
@@ -74,17 +62,17 @@ public class AddRiceTimer extends Activity implements SeekBar.OnSeekBarChangeLis
     }
 
     @Shortcut(id = "startRiceId", icon = R.drawable.myshape, shortLabel = "Long grain rice")
-    public void startRice() {
+    private void startRice() {
         try {
             Log.v("Rice timer", "Started rice timer");
             setTimer("Boil rice", BRING_RICE_TO_BOIL);
-            Thread.sleep(1000);
+            Thread.sleep(SLEEP_TIME_MILLIS);
             // Set the 15 min timer
             setTimer("Cook rice", COOK_RICE);
-            Thread.sleep(1000);
+            Thread.sleep(SLEEP_TIME_MILLIS);
             // Rest rice
             setTimer("Rest rice", REST_RICE);
-            Thread.sleep(1000);
+            Thread.sleep(SLEEP_TIME_MILLIS);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -94,44 +82,6 @@ public class AddRiceTimer extends Activity implements SeekBar.OnSeekBarChangeLis
         // Set the 3.5 min timer
         startRice();
     }
-/*    public void startJasmine(View v) {
-        Intent i =new Intent(AlarmClock.ACTION_SET_TIMER);
-        i.putExtra(AlarmClock.EXTRA_LENGTH,240);
-        i.putExtra(AlarmClock.EXTRA_MESSAGE,"Jasmine rice - bring to boil - 4 min");
-        i.putExtra(AlarmClock.EXTRA_SKIP_UI,true);
-        startActivity(i);
-        // Set the 15 min timer
-        Intent j =new Intent(AlarmClock.ACTION_SET_TIMER);
-        j.putExtra(AlarmClock.EXTRA_LENGTH,840);
-        j.putExtra(AlarmC;lock.EXTRA_MESSAGE,"Jasmine rice 14 min");
-        j.putExtra(AlarmClock.EXTRA_SKIP_UI,true);
-        startActivity(j);
-        // Rest the rice for 10 minutes
-        Intent k =new Intent(AlarmClock.ACTION_SET_TIMER);
-        k.putExtra(AlarmClock.EXTRA_LENGTH,1440);
-        k.putExtra(AlarmClock.EXTRA_MESSAGE,"Jasmine rice resting period");
-        k.putExtra(AlarmClock.EXTRA_SKIP_UI,true);
-        startActivity(k);
-    }
-    public void startBasmati(View v) {
-        Intent i =new Intent(AlarmClock.ACTION_SET_TIMER);
-        i.putExtra(AlarmClock.EXTRA_LENGTH,300);
-        i.putExtra(AlarmClock.EXTRA_MESSAGE,"Basmati rice - bring to boil - 5 min");
-        i.putExtra(AlarmClock.EXTRA_SKIP_UI,true);
-        startActivity(i);
-        // Set the 16 min timer
-        Intent j =new Intent(AlarmClock.ACTION_SET_TIMER);
-        j.putExtra(AlarmClock.EXTRA_LENGTH,960);
-        j.putExtra(AlarmClock.EXTRA_MESSAGE,"Basmati rice 16 min");
-        j.putExtra(AlarmClock.EXTRA_SKIP_UI,true);
-        startActivity(j);
-        // Rest the rice for 10 minutes
-        Intent k =new Intent(AlarmClock.ACTION_SET_TIMER);
-        k.putExtra(AlarmClock.EXTRA_LENGTH,1560);
-        k.putExtra(AlarmClock.EXTRA_MESSAGE,"Basmati rice resting period");
-        k.putExtra(AlarmClock.EXTRA_SKIP_UI,true);
-        startActivity(k);
-    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -155,7 +105,7 @@ public class AddRiceTimer extends Activity implements SeekBar.OnSeekBarChangeLis
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        intervalTextView.setText("Every " + (int)(progress / 15) + " minutes");
+        intervalTextView.setText("Every " + progress / 15 + " minutes");
     }
 
     @Override
@@ -166,5 +116,23 @@ public class AddRiceTimer extends Activity implements SeekBar.OnSeekBarChangeLis
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
 
+    }
+
+    private void setTimer(final String message, final float minutes) {
+        singleThreadPool.submit(new Runnable() {
+            public void run() {
+                int seconds = getMinutesInSeconds(minutes);
+                Intent setTimerIntent = new Intent(AlarmClock.ACTION_SET_TIMER);
+                setTimerIntent.putExtra(AlarmClock.EXTRA_LENGTH, seconds);
+                setTimerIntent.putExtra(AlarmClock.EXTRA_MESSAGE, message);
+                setTimerIntent.putExtra(AlarmClock.EXTRA_SKIP_UI, true);
+                startActivityForResult(setTimerIntent, seconds);
+                Log.v("Rice timer", "Set timer of len " + seconds + " seconds with message " + message);
+            }
+        });
+    }
+
+    private int getMinutesInSeconds(float minutes) {
+        return (int)(minutes * SECONDS_IN_MINUTE);
     }
 }
--- app/src/main/java/com/jlokimlin/rice_timer/rice_timer/Timer.java
+++ app/src/main/java/com/jlokimlin/rice_timer/rice_timer/Timer.java
@@ -7,7 +7,7 @@ import android.provider.AlarmClock;
  * Created by jlokimlin on 4/22/17.
  */
 
-public class Timer {
+class Timer {
     private String message;
     private long lengthInSeconds;
     private long timeSet;
